e2974d30f53ed54e00dfae93d4523eaeb95cdda0
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-08-22 14:02:13 -0700

    minor edits

--- models/user.py
+++ models/user.py
@@ -3,16 +3,17 @@
 from models.base_model import BaseModel
 from os import getenv
 from sqlalchemy import Column, String
+import models
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = users
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
     else:
         email = ""
         password = ""
