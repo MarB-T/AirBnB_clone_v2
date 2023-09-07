37884789f7655e279713c1b9727f6e8c83da31c5
MarB-T <danmarbet@gmail.com>
2023-09-07 22:55:11 +0300

    deploy file correct typo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -32,5 +32,5 @@ def do_deploy(archive_path):
         run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
             .format(archive_base))
         return True
-    Except Exception:
+    except Exception:
         return False
