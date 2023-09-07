c59f5d643f7788fa630540438da660e4b35bee84
MarB-T <danmarbet@gmail.com>
2023-09-07 20:37:45 +0300

    Script to archive web_static contents syntax correction

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,7 +12,7 @@ def do_pack():
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     if not (os.path.isdir("versions")):
         local("mkdir versions")
-    archive_path = "versions/web_static_{}".format(timestamp)
+    archive_path = "versions/web_static_{}.tgz".format(timestamp)
     result = local("tar -czvf {} web_static".format(archive_path))
 
     if (result.failed):
