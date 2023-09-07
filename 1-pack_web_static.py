90402b31f798ca76e4a8b0dd513fc867dccdff5f
MarB-T <danmarbet@gmail.com>
2023-09-07 20:17:04 +0300

    Script to archive web_static contents correction

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,16 +4,18 @@ Script that generates a .tgz archive from the contents of web_static
 """
 from fabric.api import local
 from datetime import datetime
+import os
 
 
 def do_pack():
     """ generate .tgz achive from web_static """
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive_name = "web_static_{}".format(timestamp)
-    local("mkdir -p versions")
-    results = local("tar -czvfn versions/{} web_static".format(archive_name))
+    if not (os.path.isdir("versions")):
+        local("mkdir versions")
+    archive_path = "versions/web_static_{}".format(timestamp)
+    results = local("tar -czvfn {} web_static".format(archive_path))
 
     if results.succeeded:
-        return "versions/{}".format(archive_name)
+        return (archive_path)
     else:
-        return None
+        return (None)
