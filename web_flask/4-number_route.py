1adf787a6cc3d58db896e6e9db5dac5439cc5891
MarB-T <danmarbet@gmail.com>
2023-09-24 12:58:46 +0300

    Using number(int) variable correction

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -39,6 +39,7 @@ def py_text(text="is cool"):
 @app.route("/number/<n>", strict_slashes=False)
 def num_route(n):
     """ Tell if n is an integer """
+    n = int(n)
     if isinstance(n, int):
         return "{} is a number".format(n)
 
