9003e3071149a75f87cc46869a7155c2875a093c
MarB-T <danmarbet@gmail.com>
2023-09-24 10:35:20 +0300

    Flask web Framework Initialization

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """
 script to start a Flask web App
 """
@@ -11,4 +11,8 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello():
     """ Display Hello HBNH! """
-    return ("<p>Hello HBNB!</p>")
+    return "<p>Hello HBNB!</p>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
