e78c0596eb27a5d4a06a0d2e3e3d63cb2817eb60
MarB-T <danmarbet@gmail.com>
2023-09-24 12:27:55 +0300

    Using text variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+script to start a Flask web App
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ Display Hello HBNH! """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Displays HBNB """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """ Display C followed by the value of the text variable """
+    text = text.replace("_", " ")
+    return f"<p>C {text}</p>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
