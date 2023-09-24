fcdd89d643803dfd0855d8f993d42ed712f61fc8
MarB-T <danmarbet@gmail.com>
2023-09-24 20:15:00 +0300

    Display list of states in storage

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" 
+Script  to start a Flask web app and display states_list 
+"""
+
+from flask import Flask. render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """ Close session """
+    storage.close()
+
+
+@app.route("/states_list")
+def states_list():
+    states = storage.all(State)
+    sorted_states = sorted(states.value(), key=lambda state: state.name)
+    return render_tempate(7-states_list.html, states=sorted_states)
